d0303203e93073add995246059066c322a4fde2f
Hasan <hshamiye@andrew.cmu.edu>
2020-04-08 13:05:39 +0000

    added user model

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+    # Relationships
+    belongs_to :band
+    has_secure_password
+    
+    # Validations
+    validates_presence_of :first_name, :last_name, :email
+    
+    # Format
+    validates_uniqueness_of :email
+    validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, message: "is not a valid format"    
+    
+    # Methods
+    def proper_name 
+        return first_name+last_name
+    end
+    
+    def self.authenticate(email,password)
+        find_by_email(email).try(:authenticate, password)
+    end
+end
---
+++ test/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    first_name "MyString"
+    last_name "MyString"
+    email "MyString"
+    role "MyString"
+    password_digest "MyString"
+    band_id 1
+    active false
+  end
+end
